3d3c03a776aa600a76703ab2d8329ee86a049b0d
dilshan-49 <157507979+dilshan-49@users.noreply.github.com>
2025-04-10 16:15:51 +0530

    USB-Serial communication between Atmega32U4 and PC using LUFA library.

---
+++ USB-Serial/main.c
@@ -0,0 +1,227 @@
+
+
+/* Includes: */
+#include <avr/io.h>
+#include <avr/wdt.h>
+#include <avr/interrupt.h>
+#include <avr/power.h>
+	
+#include "Descriptors.h"
+
+#include "LUFA/Drivers/Board/LEDs.h"
+#include "LUFA/Drivers/Peripheral/Serial.h"
+#include "LUFA/Drivers/Misc/RingBuffer.h"
+#include "LUFA/Drivers/USB/USB.h"
+#include "LUFA/Platform/Platform.h"
+
+/* Macros: */
+/** LED mask for the library LED driver, to indicate that the USB interface is not ready. */
+#define LEDMASK_USB_NOTREADY      LEDS_LED1
+
+/** LED mask for the library LED driver, to indicate that the USB interface is enumerating. */
+#define LEDMASK_USB_ENUMERATING  (LEDS_LED2 | LEDS_LED3)
+
+/** LED mask for the library LED driver, to indicate that the USB interface is ready. */
+#define LEDMASK_USB_READY        (LEDS_LED2 | LEDS_LED4)
+
+/** LED mask for the library LED driver, to indicate that an error has occurred in the USB interface. */
+#define LEDMASK_USB_ERROR        (LEDS_LED1 | LEDS_LED3)
+
+/* Function Prototypes: */
+void SetupHardware(void);
+
+
+void EVENT_USB_Device_ConfigurationChanged(void);
+void EVENT_USB_Device_ControlRequest(void);
+
+void EVENT_CDC_Device_LineEncodingChanged(USB_ClassInfo_CDC_Device_t* const CDCInterfaceInfo);
+	
+	
+	
+	
+	
+	
+	/** Circular buffer to hold data from the host before it is sent to the device via the serial port. */
+static RingBuffer_t USBtoUSART_Buffer;
+
+/** Underlying data buffer for \ref USBtoUSART_Buffer, where the stored bytes are located. */
+static uint8_t      USBtoUSART_Buffer_Data[128];
+
+/** Circular buffer to hold data from the serial port before it is sent to the host. */
+static RingBuffer_t USARTtoUSB_Buffer;
+
+/** Underlying data buffer for \ref USARTtoUSB_Buffer, where the stored bytes are located. */
+static uint8_t      USARTtoUSB_Buffer_Data[128];
+
+
+
+/** LUFA CDC Class driver interface configuration and state information. This structure is
+ *  passed to all CDC Class driver functions, so that multiple instances of the same class
+ *  within a device can be differentiated from one another.
+ */
+USB_ClassInfo_CDC_Device_t VirtualSerial_CDC_Interface =
+	{
+		.Config =
+			{
+				.ControlInterfaceNumber         = INTERFACE_ID_CDC_CCI,
+				.DataINEndpoint                 =
+					{
+						.Address                = CDC_TX_EPADDR,
+						.Size                   = CDC_TXRX_EPSIZE,
+						.Banks                  = 1,
+					},
+				.DataOUTEndpoint                =
+					{
+						.Address                = CDC_RX_EPADDR,
+						.Size                   = CDC_TXRX_EPSIZE,
+						.Banks                  = 1,
+					},
+				.NotificationEndpoint           =
+					{
+						.Address                = CDC_NOTIFICATION_EPADDR,
+						.Size                   = CDC_NOTIFICATION_EPSIZE,
+						.Banks                  = 1,
+					},
+			},
+	};
+
+
+
+
+/** Main program entry point. This routine contains the overall program flow, including initial
+ *  setup of all components and the main program loop.
+ */
+int main(void)
+{
+	SetupHardware();
+
+	RingBuffer_InitBuffer(&USBtoUSART_Buffer, USBtoUSART_Buffer_Data, sizeof(USBtoUSART_Buffer_Data));
+	RingBuffer_InitBuffer(&USARTtoUSB_Buffer, USARTtoUSB_Buffer_Data, sizeof(USARTtoUSB_Buffer_Data));
+	
+	bool ledState = false;
+	LEDs_SetAllLEDs(LEDMASK_USB_NOTREADY);
+	GlobalInterruptEnable();
+
+for (;;)
+{
+	// Check if data is available from the PC
+	int16_t ReceivedByte = CDC_Device_ReceiveByte(&VirtualSerial_CDC_Interface);
+	if (ReceivedByte >= 0)
+	{
+		// Echo the received character back to the PC
+		CDC_Device_SendByte(&VirtualSerial_CDC_Interface, ReceivedByte);
+
+		// Toggle the built-in LED (PB0)
+		ledState = !ledState;
+		if (ledState)
+		PORTC |= (1 << PORTC7); // Turn LED on
+		else
+		PORTC &= ~(1 << PORTC7); // Turn LED off
+	}
+
+	CDC_Device_USBTask(&VirtualSerial_CDC_Interface);
+	USB_USBTask();
+}
+
+}
+
+/** Configures the board hardware and chip peripherals for the demo's functionality. */
+void SetupHardware(void)
+{
+#if (ARCH == ARCH_AVR8)
+	/* Disable watchdog if enabled by bootloader/fuses */
+	MCUSR &= ~(1 << WDRF);
+	wdt_disable();
+
+	/* Disable clock division */
+	clock_prescale_set(clock_div_1);
+#endif
+
+	/* Hardware Initialization */
+	LEDs_Init();
+	USB_Init();
+	
+	DDRC |= (1 << DDC7);
+	PORTC &= ~(1 << PORTC7); // Ensure LED is off initially
+}
+
+/** Event handler for the library USB Connection event. */
+
+/** Event handler for the library USB Configuration Changed event. */
+void EVENT_USB_Device_ConfigurationChanged(void)
+{
+	bool ConfigSuccess = true;
+
+	ConfigSuccess &= CDC_Device_ConfigureEndpoints(&VirtualSerial_CDC_Interface);
+}
+
+/** Event handler for the library USB Control Request reception event. */
+void EVENT_USB_Device_ControlRequest(void)
+{
+	CDC_Device_ProcessControlRequest(&VirtualSerial_CDC_Interface);
+}
+
+/** ISR to manage the reception of data from the serial port, placing received bytes into a circular buffer
+ *  for later transmission to the host.
+ */
+ISR(USART1_RX_vect, ISR_BLOCK)
+{
+	uint8_t ReceivedByte = UDR1;
+
+	if ((USB_DeviceState == DEVICE_STATE_Configured) && !(RingBuffer_IsFull(&USARTtoUSB_Buffer)))
+	  RingBuffer_Insert(&USARTtoUSB_Buffer, ReceivedByte);
+}
+
+/** Event handler for the CDC Class driver Line Encoding Changed event.
+ *
+ *  \param[in] CDCInterfaceInfo  Pointer to the CDC class interface configuration structure being referenced
+ */
+void EVENT_CDC_Device_LineEncodingChanged(USB_ClassInfo_CDC_Device_t* const CDCInterfaceInfo)
+{
+	uint8_t ConfigMask = 0;
+
+	switch (CDCInterfaceInfo->State.LineEncoding.ParityType)
+	{
+		case CDC_PARITY_Odd:
+			ConfigMask = ((1 << UPM11) | (1 << UPM10));
+			break;
+		case CDC_PARITY_Even:
+			ConfigMask = (1 << UPM11);
+			break;
+	}
+
+	if (CDCInterfaceInfo->State.LineEncoding.CharFormat == CDC_LINEENCODING_TwoStopBits)
+	  ConfigMask |= (1 << USBS1);
+
+	switch (CDCInterfaceInfo->State.LineEncoding.DataBits)
+	{
+		case 6:
+			ConfigMask |= (1 << UCSZ10);
+			break;
+		case 7:
+			ConfigMask |= (1 << UCSZ11);
+			break;
+		case 8:
+			ConfigMask |= ((1 << UCSZ11) | (1 << UCSZ10));
+			break;
+	}
+
+	/* Keep the TX line held high (idle) while the USART is reconfigured */
+	PORTD |= (1 << 3);
+
+	/* Must turn off USART before reconfiguring it, otherwise incorrect operation may occur */
+	UCSR1B = 0;
+	UCSR1A = 0;
+	UCSR1C = 0;
+
+	/* Set the new baud rate before configuring the USART */
+	UBRR1  = SERIAL_2X_UBBRVAL(CDCInterfaceInfo->State.LineEncoding.BaudRateBPS);
+
+	/* Reconfigure the USART in double speed mode for a wider baud rate range at the expense of accuracy */
+	UCSR1C = ConfigMask;
+	UCSR1A = (1 << U2X1);
+	UCSR1B = ((1 << RXCIE1) | (1 << TXEN1) | (1 << RXEN1));
+
+	/* Release the TX line after the USART has been reconfigured */
+	PORTD &= ~(1 << 3);
+}

ca6b41ac083288ee870bf68e8c11cb16a9df577f
dilshan-49 <157507979+dilshan-49@users.noreply.github.com>
2025-04-11 18:41:36 +0530

    removed some functions. still  working

--- USB-Serial/main copy(working).c
+++ USB-Serial/main copy(working).c
@@ -14,18 +14,7 @@
 #include "LUFA/Drivers/USB/USB.h"
 #include "LUFA/Platform/Platform.h"
 
-/* Macros: */
-/** LED mask for the library LED driver, to indicate that the USB interface is not ready. */
-#define LEDMASK_USB_NOTREADY      LEDS_LED1
 
-/** LED mask for the library LED driver, to indicate that the USB interface is enumerating. */
-#define LEDMASK_USB_ENUMERATING  (LEDS_LED2 | LEDS_LED3)
-
-/** LED mask for the library LED driver, to indicate that the USB interface is ready. */
-#define LEDMASK_USB_READY        (LEDS_LED2 | LEDS_LED4)
-
-/** LED mask for the library LED driver, to indicate that an error has occurred in the USB interface. */
-#define LEDMASK_USB_ERROR        (LEDS_LED1 | LEDS_LED3)
 
 /* Function Prototypes: */
 void SetupHardware(void);
@@ -33,11 +22,6 @@ void SetupHardware(void);
 
 void EVENT_USB_Device_ConfigurationChanged(void);
 void EVENT_USB_Device_ControlRequest(void);
-
-void EVENT_CDC_Device_LineEncodingChanged(USB_ClassInfo_CDC_Device_t* const CDCInterfaceInfo);
-	
-	
-	
 	
 	
 	
@@ -98,8 +82,6 @@ int main(void)
 	RingBuffer_InitBuffer(&USBtoUSART_Buffer, USBtoUSART_Buffer_Data, sizeof(USBtoUSART_Buffer_Data));
 	RingBuffer_InitBuffer(&USARTtoUSB_Buffer, USARTtoUSB_Buffer_Data, sizeof(USARTtoUSB_Buffer_Data));
 	
-	bool ledState = false;
-	LEDs_SetAllLEDs(LEDMASK_USB_NOTREADY);
 	GlobalInterruptEnable();
 
 for (;;)
@@ -112,11 +94,8 @@ for (;;)
 		CDC_Device_SendByte(&VirtualSerial_CDC_Interface, ReceivedByte);
 
 		// Toggle the built-in LED (PB0)
-		ledState = !ledState;
-		if (ledState)
-		PORTC |= (1 << PORTC7); // Turn LED on
-		else
-		PORTC &= ~(1 << PORTC7); // Turn LED off
+
+		PORTC ^= ~(1 << PORTC7); 
 	}
 
 	CDC_Device_USBTask(&VirtualSerial_CDC_Interface);
@@ -172,56 +151,4 @@ ISR(USART1_RX_vect, ISR_BLOCK)
 	  RingBuffer_Insert(&USARTtoUSB_Buffer, ReceivedByte);
 }
 
-/** Event handler for the CDC Class driver Line Encoding Changed event.
- *
- *  \param[in] CDCInterfaceInfo  Pointer to the CDC class interface configuration structure being referenced
- */
-void EVENT_CDC_Device_LineEncodingChanged(USB_ClassInfo_CDC_Device_t* const CDCInterfaceInfo)
-{
-	uint8_t ConfigMask = 0;
 
-	switch (CDCInterfaceInfo->State.LineEncoding.ParityType)
-	{
-		case CDC_PARITY_Odd:
-			ConfigMask = ((1 << UPM11) | (1 << UPM10));
-			break;
-		case CDC_PARITY_Even:
-			ConfigMask = (1 << UPM11);
-			break;
-	}
-
-	if (CDCInterfaceInfo->State.LineEncoding.CharFormat == CDC_LINEENCODING_TwoStopBits)
-	  ConfigMask |= (1 << USBS1);
-
-	switch (CDCInterfaceInfo->State.LineEncoding.DataBits)
-	{
-		case 6:
-			ConfigMask |= (1 << UCSZ10);
-			break;
-		case 7:
-			ConfigMask |= (1 << UCSZ11);
-			break;
-		case 8:
-			ConfigMask |= ((1 << UCSZ11) | (1 << UCSZ10));
-			break;
-	}
-
-	/* Keep the TX line held high (idle) while the USART is reconfigured */
-	PORTD |= (1 << 3);
-
-	/* Must turn off USART before reconfiguring it, otherwise incorrect operation may occur */
-	UCSR1B = 0;
-	UCSR1A = 0;
-	UCSR1C = 0;
-
-	/* Set the new baud rate before configuring the USART */
-	UBRR1  = SERIAL_2X_UBBRVAL(CDCInterfaceInfo->State.LineEncoding.BaudRateBPS);
-
-	/* Reconfigure the USART in double speed mode for a wider baud rate range at the expense of accuracy */
-	UCSR1C = ConfigMask;
-	UCSR1A = (1 << U2X1);
-	UCSR1B = ((1 << RXCIE1) | (1 << TXEN1) | (1 << RXEN1));
-
-	/* Release the TX line after the USART has been reconfigured */
-	PORTD &= ~(1 << 3);
-}
